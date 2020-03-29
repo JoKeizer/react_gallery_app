c310bc1d39162e1c34cdde525fe02b122922ee99
Jo Keizer <jolanda@sociossports.nl>
2020-03-29 13:22:58 +0200

    fixed merge confilcs

---
+++ build/precache-manifest.027ae1525cc15710a94cd3923be4b1e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45135c8fc2237232686e32d93467cbc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557423a8a92e0e2dc8f8",
+    "url": "/static/css/main.24d8fae6.chunk.css"
+  },
+  {
+    "revision": "c117faf24b08ee8b14ff",
+    "url": "/static/js/2.d0ca57da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0ca57da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557423a8a92e0e2dc8f8",
+    "url": "/static/js/main.b16823ed.chunk.js"
+  },
+  {
+    "revision": "828be251ef48428c36ae",
+    "url": "/static/js/runtime-main.825a8d7d.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -13,103 +13,103 @@ import './App.css';
 
 export default class App extends Component {
 
-  //State content that will update the DOM
-  constructor() {
-    super();
-    this.state = {
-      photos: [],
-      query: '',
-      loading: true,
-      home: {
-        photos: [],
-        loading: true,
-      },
-      friends: {
-        photos: [],
-        loading: true,
-      },
-      people: {
-        photos: [],
-        loading: true,
-      },
-      sunset: {
-        photos: [],
-        loading: true,
-      }
-    };  
-  };
+    //State content that will update the DOM
+    constructor() {
+        super();
+        this.state = {
+            photos: [],
+            query: '',
+            loading: true,
+            home: {
+                photos: [],
+                loading: true,
+            },
+            friends: {
+                photos: [],
+                loading: true,
+            },
+            people: {
+                photos: [],
+                loading: true,
+            },
+            sunset: {
+                photos: [],
+                loading: true,
+            }
+        };
+    };
 
-  //ComponentDidMount will always run with axios we will get the date from Flickr API
-  componentDidMount() {
-    this.performSearch();
+    //ComponentDidMount will always run with axios we will get the date from Flickr API
+    componentDidMount() {
+        this.performSearch();
 
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=vacation&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
-    .then(response => {
-            let home ={...this.state.home};
-              home.photos = response.data.photos.photo;
-              home.loading= false;
-              this.setState({home});
-        })
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=vacation&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
+            .then(response => {
+                let home ={...this.state.home};
+                home.photos = response.data.photos.photo;
+                home.loading= false;
+                this.setState({home});
+            })
 
         axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=friends&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
-        .then(response => {
+            .then(response => {
                 let friends ={...this.state.friends};
-                  friends.photos = response.data.photos.photo;
-                  friends.loading= false;
-                  this.setState({friends});
+                friends.photos = response.data.photos.photo;
+                friends.loading= false;
+                this.setState({friends});
             })
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=people&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
-          .then(response => {
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=people&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
+            .then(response => {
                 let people ={...this.state.people};
                 people.photos = response.data.photos.photo;
                 people.loading= false;
-                      this.setState({people});
+                this.setState({people});
+            })
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=sunset&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
+            .then(response => {
+                let sunset ={...this.state.sunset};
+                sunset.photos = response.data.photos.photo;
+                sunset.loading= false;
+                this.setState({sunset});
+            })
+            .catch(error => {
+                console.log('Error fetching data')
+            })
+    }
+
+    performSearch = (query) => {
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
+            .then(response => {
+                this.setState({
+                    photos: response.data.photos.photo,
+                    loading: false,
+                    title: query
                 })
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=sunset&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
-          .then(response => {
-                 let sunset ={...this.state.sunset};
-                 sunset.photos = response.data.photos.photo;
-                 sunset.loading= false;
-                      this.setState({sunset});
-                    })
-    .catch(error => {
-        console.log('Error fetching data')
-    })
-  }
+            })
 
-  performSearch = (query) => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1&safe_search=1&safe_search=1`)
-    .then(response => {
-        this.setState({
-            photos: response.data.photos.photo,
-            loading: false,
-            title: query
-        })
-      })
+            .catch(error => {
+                console.log('Error fetching data')
+            })
+    }
 
-    .catch(error => {
-        console.log('Error fetching data')
-    })
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="container">
-          <SearchForm 
-            onSearch={this.performSearch}
-          />
-          <Nav/>
-          <Switch>
-            <Route exact path='/' render={ () => <Gallary data={this.state.home.photos} title={'vacation'} />} ></Route>
-            <Route exact path='/friends' render={ () => <Gallary data={this.state.friends.photos} title={'friends'} />} ></Route>
-            <Route exact path='/people' render={ () => <Gallary data={this.state.people.photos} title={"people"} />} ></Route>
-            <Route exact path='/sunset' render={ () => <Gallary data={this.state.sunset.photos} title={"sunset"} />} ></Route>
-            <Route exact path='/search/:query' render={ () => <Gallary data={this.state.photos} title={this.state.title} />} ></Route>
-            <Route component = {NotFound}/>                                               
-          </Switch>
-          </div>
-      </BrowserRouter>      
+    render() {
+        return (
+            <BrowserRouter>
+            <div className="container">
+            <SearchForm
+        onSearch={this.performSearch}
+        />
+        <Nav/>
+        <Switch>
+        <Route exact path='/' render={ () => <Gallary data={this.state.home.photos} title={'vacation'} />} ></Route>
+        <Route exact path='/friends' render={ () => <Gallary data={this.state.friends.photos} title={'friends'} />} ></Route>
+        <Route exact path='/people' render={ () => <Gallary data={this.state.people.photos} title={"people"} />} ></Route>
+        <Route exact path='/sunset' render={ () => <Gallary data={this.state.sunset.photos} title={"sunset"} />} ></Route>
+        <Route exact path='/search/:query' render={ () => <Gallary data={this.state.photos} title={this.state.title} />} ></Route>
+        <Route component = {NotFound}/>
+        </Switch>
+        </div>
+        </BrowserRouter>
     );
-  }
+    }
 }
