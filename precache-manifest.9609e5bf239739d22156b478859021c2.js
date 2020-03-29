5f8eca03f45037b6cdeeef849450aaaa16216027
Jo Keizer <jolanda@sociossports.nl>
2020-03-29 11:29:44 +0200

    Updates

---
+++ precache-manifest.9609e5bf239739d22156b478859021c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8798425827525f2471a6c91af86e6142",
+    "url": "/react_gallery_app/index.html"
+  },
+  {
+    "revision": "1033e9253eb810c3aa16",
+    "url": "/react_gallery_app/static/css/main.44ef5935.chunk.css"
+  },
+  {
+    "revision": "f99241a3ce645fecc984",
+    "url": "/react_gallery_app/static/js/2.1de388e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_gallery_app/static/js/2.1de388e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1033e9253eb810c3aa16",
+    "url": "/react_gallery_app/static/js/main.15d1189f.chunk.js"
+  },
+  {
+    "revision": "0e2b926bdd9e8027e0be",
+    "url": "/react_gallery_app/static/js/runtime-main.9170c2b1.js"
+  }
+]);
